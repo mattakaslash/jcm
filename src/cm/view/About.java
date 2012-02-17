b539a408084799c9b8086abc20dd3a75628925a7
Matthew Rinehart <gomamon2k@yahoo.com>
2012-02-16 20:50:21 -0500

    added hyperlink to about page; doesn't seem to work in Linux, but committing change anyway in case it works on Windows :)

--- src/cm/view/About.java
+++ src/cm/view/About.java
@@ -1,14 +1,21 @@
 package cm.view;
 
 import java.awt.Color;
+import java.awt.Desktop;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextPane;
 import javax.swing.SwingConstants;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
 
 import org.dyno.visual.swing.layouts.Bilateral;
 import org.dyno.visual.swing.layouts.Constraints;
@@ -21,7 +28,7 @@ public class About extends JDialog {
 	private static final long serialVersionUID = 1L;
 	private JLabel jLabelTitle;
 	private JLabel jLabelVersion;
-	private JLabel jLabelDescription;
+	private JTextPane jTextPaneDescription;
 	private JButton jButtonOK;
 	public About() {
 		initComponents();
@@ -36,7 +43,7 @@ public class About extends JDialog {
 		setLayout(new GroupLayout());
 		add(getJLabelTitle(), new Constraints(new Bilateral(0, 0, 41), new Leading(0, 41, 12, 12)));
 		add(getJLabelVersion(), new Constraints(new Bilateral(0, 1, 43), new Leading(47, 12, 12)));
-		add(getJLabelDescription(), new Constraints(new Bilateral(0, 1, 41), new Leading(85, 75, 10, 10)));
+		add(getJTextPaneDescription(), new Constraints(new Bilateral(0, 1, 41), new Leading(85, 75, 10, 10)));
 		add(getJButtonOK(), new Constraints(new Bilateral(261, 262, 81), new Leading(166, 12, 12)));
 		pack();
 	}
@@ -55,15 +62,26 @@ public class About extends JDialog {
 		return jButtonOK;
 	}
 
-	private JLabel getJLabelDescription() {
-		if (jLabelDescription == null) {
-			jLabelDescription = new JLabel();
-			jLabelDescription.setText("<html>DnD 4e Combat Manager is based on the program of the same name hosted at<br>" +
-					"http://www.dragonpro.com/index.php?option=com_jdownloads&view=viewcategory&catid=2&Itemid=53.<br>" +
+	private JTextPane getJTextPaneDescription() {
+		if (jTextPaneDescription == null) {
+			jTextPaneDescription = new JTextPane();
+			jTextPaneDescription.setContentType("text/html");
+			jTextPaneDescription.setBackground(this.getBackground());
+			jTextPaneDescription.setEditable(false);
+			jTextPaneDescription.setText("<html>DnD 4e Combat Manager is based on the program of the same name hosted at<br>" +
+					"<a href=\"http://www.dragonpro.com/index.php?option=com_jdownloads&view=viewcategory&catid=2&Itemid=53\">" +
+					"http://www.dragonpro.com/index.php?option=com_jdownloads&view=viewcategory&catid=2&Itemid=53</a>.<br>" +
 					"This is a Java port of an early source release, with some features from the original added and<br>" +
 					"other unique features implemented also.");
+			jTextPaneDescription.addHyperlinkListener(new HyperlinkListener() {
+				
+				@Override
+				public void hyperlinkUpdate(HyperlinkEvent event) {
+					jTextPaneDescriptionHyperlinkHyperlinkUpdate(event);
+				}
+			});
 		}
-		return jLabelDescription;
+		return jTextPaneDescription;
 	}
 
 	private JLabel getJLabelVersion() {
@@ -88,5 +106,25 @@ public class About extends JDialog {
 
 	private void jButtonOKActionActionPerformed(ActionEvent event) {
 		this.setVisible(false);
+	}
+	
+	private void jTextPaneDescriptionHyperlinkHyperlinkUpdate(HyperlinkEvent event) {
+		if (event.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+			try {
+				URI uri = new URI(event.getDescription());
+				if (Desktop.isDesktopSupported()) {
+					Desktop.getDesktop().browse(uri);
+				}
+			} catch (IOException e) {
+				JOptionPane.showMessageDialog(null,
+						"Unable to launch your default browser: "
+								+ e.getLocalizedMessage(),
+						"Error Launching Browser",
+						JOptionPane.ERROR_MESSAGE);
+				e.printStackTrace();
+			} catch (URISyntaxException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
