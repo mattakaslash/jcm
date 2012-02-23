c99baec2e432c9f4bb0f56637fea6e5272cf71f8
matthew.rinehart <matthew.rinehart@navy.mil>
2012-02-23 14:44:54 -0500

    rc3!

--- src/cm/DnD4eCombatManager.java
+++ src/cm/DnD4eCombatManager.java
@@ -13,7 +13,7 @@ public class DnD4eCombatManager {
 	/**
 	 * Application version.
 	 */
-	public static String VERSION = "1.0RC2";
+	public static String VERSION = "1.0RC3";
 
 	/**
 	 * Invokes the main frame.
