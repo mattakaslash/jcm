d388fad5f84cb24360013f38c27ac4013e474325
matthew.rinehart <matthew.rinehart@navy.mil>
2012-02-28 09:20:16 -0500

    releasing version 1.0

--- src/cm/DnD4eCombatManager.java
+++ src/cm/DnD4eCombatManager.java
@@ -13,7 +13,7 @@ public class DnD4eCombatManager {
 	/**
 	 * Application version.
 	 */
-	public static String VERSION = "1.0RC3";
+	public static String VERSION = "1.0";
 
 	/**
 	 * Invokes the main frame.
