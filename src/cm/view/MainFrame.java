5ee8bddad8e252b7fe3d96142fabd028394850ab
matthew.rinehart <matthew.rinehart@navy.mil>
2012-05-10 13:15:14 -0400

    re-implemented close of init display on encounter end

--- src/cm/view/MainFrame.java
+++ src/cm/view/MainFrame.java
@@ -3253,6 +3253,8 @@ public class MainFrame extends JFrame {
 				JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
 
 		if (n == JOptionPane.YES_OPTION) {
+			getMenuWindowsMinimalInitDisplay().setSelected(false);
+			getMenuWindowsFullInitDisplay().setSelected(false);
 			getJTableRoster().clearSelection();
 			getFight().resetEncounter(false);
 			getJTabbedPaneUtils().setSelectedIndex(0);
